a01d6bdf812a3acb0b614e27b279b2e11b90b5d8
Josimar Lima <osimar.lima2007@gmail.com>
2020-07-14 16:30:41 -0300

    initial commit gobarber-api

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e0f2fe8482c616725ccd80726af5a200',
+    expiresIn: '1d',
+  },
+};
---
+++ src/database/migrations/1592664435547-alterProviderFieldToProviderId.ts
@@ -0,0 +1,44 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class alterProviderFieldToProviderId1592664435547
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'appointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'appointmentProvider');
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
+}
