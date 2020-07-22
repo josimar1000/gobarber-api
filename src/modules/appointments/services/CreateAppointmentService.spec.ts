46eacf53b100899009f079a6a0a1f2bc8df042ea
Josimar Lima <osimar.lima2007@gmail.com>
2020-07-22 00:16:33 -0300

    creating the firist test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,19 @@
-test('sum two numbers', () => {
-  expect(1 + 3).toBe(4);
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('Should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
 });
